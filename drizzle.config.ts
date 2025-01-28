cb92a7e1f84cd01657c36f12899806d6cdff3acb
Victor Otávio Ferreira <victor@ferreira.id>
2025-01-28 17:24:35 -0300

    feat: install and setup Drizzle ORM

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./drizzle/schema.ts",
+  dbCredentials: {
+    url: String(process.env.DATABASE_URL),
+  },
+  verbose: true,
+  strict: true,
+});
---
+++ drizzle/schema.ts
@@ -0,0 +1,24 @@
+import {
+  integer,
+  pgTable,
+  serial,
+  smallint,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const songs = pgTable("songs", {
+  id: serial("id").primaryKey(),
+  artist: varchar("artist", { length: 100 }).notNull(),
+  title: varchar("title", { length: 100 }).notNull(),
+  albumTitle: varchar("album_title", { length: 100 }),
+  albumYear: smallint(),
+  cover: varchar("cover", { length: 256 }),
+  poster: varchar("poster", { length: 256 }),
+  audio: varchar("audio", { length: 256 }),
+});
+
+export const relatedSongs = pgTable("related_songs", {
+  id: serial("id").primaryKey(),
+  songId: integer("song_id").references(() => songs.id),
+  relatedSongId: integer("related_song_id").references(() => songs.id),
+});
